24407148db94d4f62db0a9c1638f431affbc4a32
Nikola Bebic <bebicnikola@gmail.com>
2018-06-11 15:42:51 +0200

    Refactor factory

--- src/implementation/CertificateCreator.java
+++ src/implementation/CertificateCreator.java
@@ -9,7 +9,6 @@ import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x509.*;
 import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
@@ -29,8 +28,8 @@ import java.security.cert.X509Certificate;
 import java.util.Date;
 import java.util.HashMap;
 
-public final class CertificateFactory {
-    private CertificateFactory() {}
+public final class CertificateCreator {
+    private CertificateCreator() {}
 
     private static final HashMap<Integer, Integer> KEY_USAGES = new HashMap<>();
     static {
